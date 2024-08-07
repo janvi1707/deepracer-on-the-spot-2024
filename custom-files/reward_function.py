a03ef765bac1b8e2f3a3fd24099d2a1e4f2fb227
subhashkay90787 <subhashkay907871@gmail.com>
2024-08-07 21:54:19 +0530

    Update reward_function.py

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -47,7 +47,7 @@ def fetch_required_steering_angle(waypoints,closest_waypoints,x,y,track_width,he
     
     ideal_heading = math.atan2(next_start[1]-start[1],next_start[0]-start[0])
     ideal_heading = math.degrees(ideal_heading)
-    direction_diff = track_direction-ideal_heading
+    direction_diff = track_direction-heading
 
     if(direction_diff>180):
         direction_diff = direction_diff-360;
