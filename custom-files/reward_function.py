4a2aad2ebb08b9b23b75324c0118bbb2676fa492
janvi1707 <67873350+janvi1707@users.noreply.github.com>
2024-08-29 00:09:08 +0530

    m4

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -91,6 +91,19 @@ def closest_route_point(x,y,optimal_waypoints):
 def reward_function(params):
     curve_points=[17,18,19,20,21,22,23,24,25,26,27,28,29,30,54,55,56,57,58,59,60,61,62,97,98,99,100,101,102,103,104,105,106,107,108,109,110,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,153,154,155,156,157,158,159,160,183,184,185,186,187,188,189,190,191,192,193,194,195,196]
     straight_points=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 64, 65, 66, 67, 68, 69, 70, 71, 72, 81, 82, 83, 84, 85,86,87,88,89,90, 91, 92, 93, 94, 118, 119, 120, 127, 128, 129, 130, 131, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213]
+    
+    left_turn=[18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,55,56,57,58,59,60,61,62,63,64,77,78,79,80,81,82,
+               99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,
+               186,187,188,189,190,191,192,193,194,195,196]
+    not_very_left=[16,17,33,53,54,76,83,98,125,126,185,197,198]
+    left_mid=[13,14,15,34,50,51,52,65,75,84,97,127,128,182,183,184,199,200,201,202]
+    basic_left=[6,7,8,9,10,11,12,35,36,47,48,49,66,74,85,86,96,129,130,177,178,179,180,181,203,204,205,206,207]
+    straight_waypoints=[0,1,2,3,4,5,37,46,67,73,87,95,131,172,173,174,175,176,208,209,210,211,212,213]
+    basic_right=[38,39,44,45,68,72,88,89,90,94,132,168,169,170,171]
+    right_mid_path=[40,41,42,43,69,70,71,91,92,93,133,165,166,167]
+    not_very_right_waypoints=[134,142,143,144,163,164]
+    right_turn=[135,136,137,138,139,140,141,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162]
+    
     # Read input parameters
     heading = params['heading']
     IC=0
@@ -171,12 +184,59 @@ def reward_function(params):
         return 1e-3
 
 # ###########################################################
-#     #Heading Reward
+#     #Optimal path
 # ###########################################################
     vehicle_x = params['x']
     vehicle_y = params['y']
+    opt_reward=0
     prev_closest_point_index,next_closest_point_index = closest_route_point(vehicle_x,vehicle_y,optimal_waypoints)
+    if next in left_turn and params['is_left_of_center']:
+        opt_reward+=0.3
+        if params['distance_from_center']>=0.25*params['track_width']:
+            opt_reward+=0.7
+        elif params['distance_from_center']>=0.18*params['track_width']:
+            opt_reward+=0.4
+        elif  params['distance_from_center']>=0.1*params['track_width']:
+            opt_reward+=0.2
+    if next in left_turn and not params['is_left_of_center']:
+        opt_reward-=0.3
+    if next in right_turn and not params['is_left_of_center']:
+        opt_reward+=0.3
+        if params['distance_from_center']>=0.25*params['track_width']:
+            opt_reward+=0.7
+        elif params['distance_from_center']>=0.18*params['track_width']:
+            opt_reward+=0.4
+        elif  params['distance_from_center']>=0.1*params['track_width']:
+            opt_reward+=0.2
+    if next in right_turn and params['is_left_of_center']:
+        opt_reward-=0.3
+    if next in not_very_right_waypoints and not params['is_left_of_center']:
+        opt_reward+=0.3
+        if params['distance_from_center']>=0.2*params['track_width']:
+            opt_reward+=0.7
+    if next in not_very_left and params['is_left_of_center']:
+        opt_reward+=0.3
+        if  params['distance_from_center']>=0.2*params['track_width']:
+            opt_reward+=0.7
+    if next in basic_left:
+        if params['is_left_of_center'] or params['distance_from_center']==0:
+            opt_reward+=0.3
+            if params['distance_from_center']<=0.2*params['track_width']:
+                opt_reward+=0.7
+    if next in basic_right:
+        if not params['is_left_of_center'] or params['distance_from_center']==0:
+            opt_reward+=0.3
+            if params['distance_from_center']<=0.2*params['track_width']:
+                opt_reward+=0.7
+    if next in right_mid_path and not params['is_left_of_center']:
+        opt_reward+=0.3
+        if params['distance_from_center']>=0.18*params['track_width'] and params['distance_from_center']<0.25*params['track_width']:
+            opt_reward+=0.7
 
+    if next in left_mid and params['is_left_of_center']:
+        opt_reward+=0.3
+        if params['distance_from_center']>=0.18*params['track_width'] and params['distance_from_center']<0.25*params['track_width']:
+            opt_reward+=0.7
 ###########################################################
     #Speed Reward
 ###########################################################
@@ -219,8 +279,10 @@ def reward_function(params):
     HC = ( 10 * steering_reward )
     #speed component of reward
     SC = ( 5 * speed_reward * speed_maintain_bonus )
+
+    OC = 5 * opt_reward
     #Immediate component of reward
-    IC = ( HC + SC ) ** 2 + ( HC * SC )
+    IC = ( HC + SC +OC ) ** 2 + ( HC * SC * OC )
 
     print("Speed Maintain Bonus :",speed_maintain_bonus)
 #If an unpardonable action is taken, then the immediate reward is 0
